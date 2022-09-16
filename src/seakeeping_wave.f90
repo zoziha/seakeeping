a82414cef4f890418397eb1b47101675bf373f7b
zoziha <zuo.zhihua@qq.com>
2022-09-16 13:56:14 +0800

    Add k01, k02, wlr, wf

--- src/seakeeping_wave.f90
+++ src/seakeeping_wave.f90
@@ -10,18 +10,27 @@ module seakeeping_wave
     implicit none
     private
 
-    public :: k0, we
+    public :: k01, k02, we, wlr, wf
 
 contains
 
     !> Wave number from wave frequency <br>
     !> 波频率计算波数
-    elemental real(rk) function k0(w)
+    elemental real(rk) function k01(w) result(k0)
         real(rk), intent(in) :: w
 
         k0 = w*w/g
 
-    end function k0
+    end function k01
+
+    !> Wave number from wave length <br>
+    !> 波长计算波数
+    elemental real(rk) function k02(l) result(k0)
+        real(rk), intent(in) :: l
+
+        k0 = 2*Pi/l
+
+    end function k02
 
     !> Encountered wave frequency <br>
     !> 遭遇波频率
@@ -34,11 +43,35 @@ contains
                                                 !! 船舶航向
 
         if (present(deg)) then
-            we = w + k0(w)*v*cos(deg)
+            we = w + k01(w)*v*cos(deg)
         else
-            we = w + k0(w)*v
+            we = w + k01(w)*v
         end if
 
     end function we
+
+    !> Wavelength to length ratio <br>
+    !> 波长与船长比
+    elemental real(rk) function wlr(w, l)
+        real(rk), intent(in) :: w   !! Wave frequency <br>
+                                    !! 波频率
+        real(rk), intent(in) :: l   !! Ship length <br>
+                                    !! 船舶特征长度
+
+        wlr = 2*Pi/(k01(w)*l)
+
+    end function wlr
+
+    !> Wave frequency from wlr <br>
+    !> 由波长与船长比计算波频率
+    elemental real(rk) function wf(wlr, l)
+        real(rk), intent(in) :: wlr !! Wavelength to length ratio <br>
+                                    !! 波长与船长比
+        real(rk), intent(in) :: l   !! Ship length <br>
+                                    !! 船舶特征长度
+
+        wf = sqrt(g*k02(wlr*l))
+
+    end function wf
 
 end module seakeeping_wave
