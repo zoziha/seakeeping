57b416ed3aa80b85aac62a83d4ddc20fc4b87797
zoziha <zuo.zhihua@qq.com>
2022-09-16 14:15:16 +0800

    Add stdlib optval

--- src/seakeeping_utils.f90
+++ src/seakeeping_utils.f90
@@ -9,11 +9,15 @@ module seakeeping_utils
     implicit none
     private
 
-    public :: swap, incr
+    public :: swap, incr, optval
 
     interface incr
         procedure :: incr_ik, incr_rk
     end interface incr
+
+    interface optval
+        procedure :: optval_ik, optval_rk, optval_lk
+    end interface optval
 
 contains
 
@@ -56,5 +60,50 @@ contains
         end if
 
     end subroutine incr_ik
+
+    !> Optional value <br>
+    !> 可选值
+    !> @note 借鉴自 stdlib
+    elemental real(rk) function optval_rk(a, b) result(optval)
+        real(rk), intent(in), optional :: a
+        real(rk), intent(in) :: b
+
+        if (present(a)) then
+            optval = a
+        else
+            optval = b
+        end if
+
+    end function optval_rk
+
+    !> Optional value <br>
+    !> 可选值
+    !> @note 借鉴自 stdlib
+    elemental integer function optval_ik(a, b) result(optval)
+        integer, intent(in), optional :: a
+        integer, intent(in) :: b
+
+        if (present(a)) then
+            optval = a
+        else
+            optval = b
+        end if
+
+    end function optval_ik
+
+    !> Optional value <br>
+    !> 可选值
+    !> @note 借鉴自 stdlib
+    elemental logical function optval_lk(a, b) result(optval)
+        logical, intent(in), optional :: a
+        logical, intent(in) :: b
+
+        if (present(a)) then
+            optval = a
+        else
+            optval = b
+        end if
+
+    end function optval_lk
 
 end module seakeeping_utils
