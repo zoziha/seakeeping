16fd5e4f40f341585e2e1a9a769752fb7935fabf
zoziha <zuo.zhihua@qq.com>
2022-10-09 11:58:51 +0800

    Update seakeeping module

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Seakeeping Package
 
+[![seakeeping](https://img.shields.io/badge/seakeeping-v1.0.0-blueviolet)][1]
+[![license](https://img.shields.io/badge/License-BSD--3-important)](LICENSE)
+[![fpm](https://img.shields.io/badge/Fortran--lang/fpm-^0.6.0-blue)][2]
+[![Compiler](https://img.shields.io/badge/Compiler-GFortran^10.3.0-brightgreen)][3]
+
+[1]: https://github.com/zoziha/seakeeping
+[2]: https://github.com/fortran-lang/fpm
+[3]: https://fortran-lang.org/compilers
+
 Based on programming experience, writing more detailed and expressive code for a specific field helps to improve the readability, maintainability, and descriptiveness of the code from the bottom.
 
 Therefore, this package is formed to express and calculate the seakeeping problem of ships.
--- src/seakeeping.f90
+++ src/seakeeping.f90
@@ -1,25 +1,28 @@
+!! 集合所有模块功能
+
 !> author: 左志华
 !> date: 2022-09-16
+!> version: release
 !>
 !> Seakeeping routines for Naval Architecture and Ocean Engineering <br>
 !> 船舶与海洋工程耐波性例程
 module seakeeping
 
-    use seakeeping_kinds
     use seakeeping_constants
+    use seakeeping_display
+    use seakeeping_error_handling
+    use seakeeping_filesystem
+    use seakeeping_geometry
+    use seakeeping_kinds
+    use seakeeping_linalg
     use seakeeping_math
+    use seakeeping_statics
+    use seakeeping_string
+    use seakeeping_time
     use seakeeping_tsa
     use seakeeping_wave
-    use seakeeping_utils
     use seakeeping_units
-    use seakeeping_geometry
-    use seakeeping_string
-    use seakeeping_filesystem
-    use seakeeping_display
-    use seakeeping_time
-    use seakeeping_error_handling
-    use seakeeping_statics
-    use seakeeping_linalg
+    use seakeeping_utils
     implicit none
 
 end module seakeeping
